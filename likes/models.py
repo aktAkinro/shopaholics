cdaa8e424a554ddf5e735f78ed600d6dc20199e2
Akinro Akintunde <aktakinro@gmail.com>
2022-01-21 03:09:02 -0800

    models

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
+from django.conf import settings
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # the three fields below are necessary for a generic relationship
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) #content type is used to create generic relationships
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
--- shopaholics/settings.py
+++ shopaholics/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',
     'core',
-    'tags'
+    'tags',
+    'likes',
 ]
 
 MIDDLEWARE = [
