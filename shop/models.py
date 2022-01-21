2dddc1fe730708819565328ff8453372c499600c
Akinro Akintunde <aktakinro@gmail.com>
2022-01-21 02:46:28 -0800

    models

--- shop/models.py
+++ shop/models.py
@@ -3,12 +3,27 @@ from django.db import models
 
 # Create your models here.
 
+#many to many reltionship between promotions and products
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+') #due to circular dependency we put + so djnago doesnt create reverse relationship
+
+
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=9, decimal_places=2) #i used decimal field here because float field has rounding issues
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT) # PROTECT IS USED SO IF YOU DELETE A COLLECTION YOU DONT DELETE PRODUCTS IN THE COLLECTION
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
@@ -47,11 +62,37 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT) # PROTECT IS USED SO THAT IF WE ACCIDENTALLY DELETE A CUSTOMER WE DONT DELETE ORDERS
 
 
-#one to one relationship between customer and address
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField() # POSITIVE SMALL INTEGER TO PREVENT NEGAAATIVE VALUES
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2) # UNIT PRICE IS STORED HERE AS WELL AS PRODUCT CLASS. SO WE CAN SAVE PRICE AT THE TIME OF ORDER
+
+
+# #one to one relationship between customer and address(ensure that primary key = True is there)
+# class Adress(models.Model):
+#     street = models.CharField(max_length=255)
+#     city = models.CharField(max_length=255)
+#     customer = models.OneToOneField(Customer, on_delete=CASCADE, primary_key=True) # with cascade you delete associated child(addrress) and parent(customer)
+
+
+# one to many relationship between customer and address (remove primary key = True and set field to foreign key)
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
