7bd664a58744d5424aec6684ced36fade8862911
Akinro Akintunde <aktakinro@gmail.com>
2022-01-21 01:12:17 -0800

    models

--- shopaholics/settings.py
+++ shopaholics/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from re import A
 from dotenv import load_dotenv, find_dotenv
 load_dotenv()
 
@@ -43,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
+    'core',
+    'tags'
 ]
 
 MIDDLEWARE = [
@@ -138,3 +142,6 @@ EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 # EMAIL_USE_SSL
+
+
+AUTH_USER_MODEL = 'core.User'
